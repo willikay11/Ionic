a3155493b300cd23b4dff488278b073255edfb0c
willikay11 <willikay11@gmail.com>
2017-09-14 08:32:11 +0300

    Pulling songs from the online server

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,16 +4,42 @@ angular.module('songhop.controllers', ['ionic', 'songhop.services'])
 /*
 Controller for the discover page
 */
-.controller('DiscoverCtrl', function($scope) {
+.controller('DiscoverCtrl', function($scope, $timeout, User, Recommendations) {
+    //Our first three songs
+    Recommendations.getNextSongs().then(function () {
+       $scope.currentSong = Recommendations.queue[0];
+    });
 
+
+    $scope.sendFeedback = function (bool) {
+
+        if(bool) User.addSongToFavorites($scope.currentSong);
+
+        $scope.currentSong.rated = bool;
+
+        $scope.currentSong.hide = true;
+
+        Recommendations.nextSong();
+
+        $timeout(function () {
+            $scope.currentSong = Recommendations.queue[0];
+        }, 250);
+
+
+    }
 })
 
 
 /*
 Controller for the favorites page
 */
-.controller('FavoritesCtrl', function($scope) {
+.controller('FavoritesCtrl', function($scope, User) {
 
+    $scope.favorites = User.favorites;
+
+    $scope.removeSong = function (song, index) {
+        User.removeSongFromFavorites(song, index);
+    }
 })
 
 
